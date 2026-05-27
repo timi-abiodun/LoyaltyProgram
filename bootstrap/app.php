473b3828efebcba7935a2d24c4c57c990a6a3853
Oluwatimilehin Abiodun <oluwatimilehinabiodun64@gmail.com>
2026-05-27 23:21:31 +0100

    Refactor app.php by removing comments and adding CORS
    Removed commented-out configuration code and added CORS middleware.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,27 +1,5 @@
 <?php
 
-// use Illuminate\Foundation\Application;
-// use Illuminate\Foundation\Configuration\Exceptions;
-// use Illuminate\Foundation\Configuration\Middleware;
-
-// return Application::configure(basePath: dirname(__DIR__))
-//     ->withRouting(
-//         web: __DIR__.'/../routes/web.php',
-//         api: __DIR__.'/../routes/api.php',
-//         commands: __DIR__.'/../routes/console.php',
-//         health: '/up',
-//     )
-//     ->withMiddleware(function (Middleware $middleware): void {
-//         //
-//     })
-//     ->withExceptions(function (Exceptions $exceptions): void {
-//         $exceptions->shouldRenderJsonWhen(function (Request $request, Throwable $e) {
-//             return $request->is('api/*');
-//         });
-//     })->create();
-
-
-
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -36,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+         $middleware->prepend(\Illuminate\Http\Middleware\HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(function (Request $request, Throwable $e) {
